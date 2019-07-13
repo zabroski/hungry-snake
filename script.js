ad37b86395bf58eb017a1790371a3688963429a0
Issouf Zabre <sheridanzabre@gmail.com>
2019-07-12 20:26:26 -0400

    enemies are in the game ohh ohh

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// let canMoveX = true;
-// let canMoveY = true;
+
+// eslint - disable;
 
 const body = document.getElementsByTagName('body')[0];
 
@@ -8,16 +8,9 @@ const grid = {
     x: 0,
     y: 0,
   },
-  foods: [{
-    id: 'food',
-    x: 7, // 350px
-    y: 7, // 350px
-  },
-  {
-    id: 'food2',
-    x: 2, // 50px
-    y: 2, // 50px
-  }],
+
+  foods: [],
+  enemy: [],
 };
 let direction = null;
 
@@ -35,29 +28,30 @@ function moveSnake(direction) {
   let newYPosition = currentYPosition;
 
   if (direction === 'ArrowRight') {
-    newXPosition = parseInt(currentXPosition) + 50;
+    newXPosition = parseInt(currentXPosition) + 50; // returning var current X position en number!
   } else if (direction === 'ArrowLeft') {
     newXPosition = parseInt(currentXPosition) - 50;
   } else if (direction === 'ArrowUp') {
-    newYPosition = parseInt(currentYPosition) - 50;
+    newYPosition = parseInt(currentYPosition) - 50; // returning variable current Y position en number!
   } else if (direction === 'ArrowDown') {
     newYPosition = parseInt(currentYPosition) + 50;
   }
 
   if (newXPosition >= 0 && newXPosition <= 350) {
-    snake.style.marginLeft = `${newXPosition}px`;
+    snake.style.marginLeft = `${newXPosition}px`; // snake box will stay in the grid cuz original g.. is 400px
     grid.snake.x = calculateItemPosition(newXPosition);
   }
 
   if (newYPosition >= 0 && newYPosition <= 350) {
     snake.style.marginTop = `${newYPosition}px`;
-    grid.snake.y = calculateItemPosition(newYPosition);
+    grid.snake.y = calculateItemPosition(newYPosition); // ensuite pour le Y aussi!
   }
 }
 
+
 function getFoodAt(x, y) {
   for (let i = 0; i < grid.foods.length; i++) {
-    if (x === grid.foods[i].x && y === grid.foods[i].y) {
+    if (x === grid.foods[i].x && y === grid.foods[i].y) { // now i know where is foods in grid
       return grid.foods[i];
     }
   }
@@ -65,30 +59,151 @@ function getFoodAt(x, y) {
   return null;
 }
 
+
 function eatFoods() {
-  const food = getFoodAt(grid.snake.x, grid.snake.y);
+  const food = getFoodAt(grid.snake.x, grid.snake.y); //  using X and Y snake to eat foods
   if (food != null) {
-    document.getElementById(food.id).remove();
+    document.getElementById(food.id).remove(); // and use ID of food to remove it from grid
   }
+}
+
+function getEnemyAt(x, y) { // / enemy part
+  for (let i = 0; i < grid.enemy.length; i++) {
+    if (x === grid.enemy[i].x && y === grid.enemy[i].y) {
+      return grid.enemy[i];
+    }
+  }
+  return null;
+}
+
+function touchDied() {
+  const enemy = getEnemyAt(grid.snake.y, grid.snake.y);
+  if (enemy === enemy) {
+    console.log('you touch me');
+  }
+}
+
+
+function isFree(x, y) {
+  if (x === grid.snake.x && y === grid.snake.y) { // need to
+    return false;
+  }
+
+  for (let i = 0; i < grid.foods.length; i++) {
+    if (x === grid.foods[i].x && y === grid.foods[i].y) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isEnemyFree(x, y) {
+  if (x === grid.x && y === grid.snake.y) { // enemy part...
+    return false;
+  }
+  for (let i = 0; i < grid.enemy.length; i++) {
+    if (x === grid.enemy[i].x && y === grid.enemy[i].y) {
+      return false;
+    }
+  }
+  return true;
 }
 
 body.addEventListener('keydown', (evt) => {
   direction = evt.code;
 });
 
-function initFood() {
-  // forloop to display div on the virtual html grid and in the var grid
+function renderEnnemy() {
+  // for loop to dispaly div on the virtual html grid and in the let grid
+  const howManyEnemy = Math.floor(Math.random() * 4);
 
-  const food = document.querySelector('body');
-  const newDiv = document.createElement('div');
-  //   const textNode = document.createTextNode('randomFood');
-  food.appendChild(newDiv);
-  newDiv.classList.add('food1');
-//   newDiv.appendChild(textNode);
+  for (let i = 0; i < renderEnnemy; i++) {
+    const id = `enemy${i}`;
+    const enemy = document.createElement('div');
+
+    const pxRandomXFoodPosition = Math.floor(Math.random() * 4);
+    const pxRandomYFoodPosition = Math.floor(Math.random() * 4);
+
+    if (isEnemyFree(pxRandomXFoodPosition, pxRandomYFoodPosition)) {
+      i--;
+    } else {
+      const pxPositionX = (50 * pxRandomXFoodPosition);
+      const pxPositionY = (50 * pxRandomYFoodPosition);
+      enemy.id = id;
+      enemy.style.marginLeft = `${pxPositionX}px`;
+      enemy.style.marginTop = `${pxPositionY}px`;
+      enemy.classList.add('food');
+
+
+      const htmlGrid = document.getElementsByClassName('grid')[0];
+      htmlGrid.appendChild.appendChild(enemy);
+      grid.enemy.push({
+        x: calculateItemPosition(pxPositionX),
+        y: calculateItemPosition(pxPositionY),
+        id,
+      });
+    }
+  }
+}
+for (let i = 0; i < 3; i++) {
+//   const id = `enemy${i}`;
+  const htmlGrid = document.getElementsByClassName('grid')[0];
+  const enemy = document.createElement('div');
+  htmlGrid.appendChild(enemy);
+  enemy.classList.add('enemy');
+  const pxRandomXFoodPosition = Math.floor(Math.random() * 4);
+  const pxRandomYFoodPosition = Math.floor(Math.random() * 4);
+  const pxPositionX = (50 * pxRandomXFoodPosition);
+  const pxPositionY = (50 * pxRandomYFoodPosition);
+  enemy.style.marginLeft = `${pxPositionX}px`;
+  enemy.style.marginRight = `${pxPositionY}px`;
+
+  grid.enemy.push({
+    x: calculateItemPosition(pxPositionX),
+    y: calculateItemPosition(pxPositionY),
+    // id,
+  });
 }
 
+
+function renderFood() {
+  //   // forloop to display div on the virtual html grid and in the let grid
+
+  const howManyFood = Math.floor(Math.random() * 7);
+  for (let i = 0; i < howManyFood; i++) { // loop in my random foods
+    const id = `food${i}`;
+    const food = document.createElement('div');
+
+    const pxRandomXFoodPosition = Math.floor(Math.random() * 7); // random position of X
+    const pxRandomYFoodPosition = Math.floor(Math.random() * 7); // random position of Y
+
+    if (!isFree(pxRandomXFoodPosition, pxRandomYFoodPosition)) {
+      i--; // keeps the for loop if space is not free
+    } else {
+      const pxPositionX = (50 * pxRandomXFoodPosition); // have my foods
+      const pxPositionY = (50 * pxRandomYFoodPosition);//
+      food.id = id;
+      food.style.marginLeft = `${pxPositionX}px`;
+      food.style.marginTop = `${pxPositionY}px`;
+      food.classList.add('food');
+
+      const htmlGrid = document.getElementsByClassName('grid')[0];
+      htmlGrid.appendChild(food);
+
+      grid.foods.push({
+        x: calculateItemPosition(pxPositionX),
+        y: calculateItemPosition(pxPositionY),
+        id,
+      });
+    }
+  }
+}
+touchDied();
+
 function startGame() {
-  initFood();
+  renderFood();
+  renderEnnemy();
   setInterval(() => { // when press direction going to setInterval witout res
     moveSnake(direction);
     eatFoods();
@@ -96,67 +211,3 @@ function startGame() {
 }
 
 startGame();
-
-
-// let foods = ['food1', 'food2', 'food3']
-
-// const moveSnake = function () {
-//   const body = document.getElementsByTagName('body')[0];
-//   body.addEventListener('keydown', (event) => {
-//     const currentXPosition = event.;
-//     let newXPosition = currentXPosition;
-
-//     const currentYPosition = event.target.style.marginTop.replace('px', '') || 0;
-//     let newYPosition = currentYPosition;
-
-//     if (event.code == 'ArrowRight') {
-//       newXPosition = parseInt(currentXPosition) + 20;
-//     } else if (event.code == 'ArrowLeft') {
-//       newXPosition = parseInt(currentXPosition) - 20;
-//     } else if (event.code == 'ArrowUp') {
-//       newYPosition = parseInt(currentYPosition) - 20;
-//     } else if (event.code == 'ArrowDown') {
-//       newYPosition = parseInt(currentYPosition) + 20;
-//     }
-//     event.target.style.marginLeft = `${newXPosition}px`;
-//     event.target.style.marginTop = `${newYPosition}px`;
-//   });
-// };
-
-
-// const createFoods = function () {
-//   const food = document.querySelector('body');
-//   const newDiv = document.createElement('div');
-//   const textNode = document.createTextNode('randomFood');
-//   food.appendChild(newDiv);
-//   newDiv.classList.add('food');
-//   newDiv.appendChild(textNode);
-// };
-
-// for (let i = 0; i < 2; i++) {
-//   createFoods();
-// };
-
-// const randomFoodFunction = function() {
-//     // for(let i = 0; i < foods.length; i++){
-//     //     // return randomFood[i];
-//     // }
-//     let randomFood = Math.floor(Math.random() * foods.length);
-//     console.log(randomFood)
-// }
-
-
-// let randomFood = Math.floor(Math.random() * foods.length);
-// console.log(randomFood)
-
-// const createMongooses = function () {
-//     const mongooses = document.querySelector('body');
-//     const newDiv = document.createElement('div');
-//     const textNode = document.createTextNode('mongooses');
-//     mongooses.appendChild(newDiv);
-//     newDiv.classList.add('mongoose');
-//     newDiv.appendChild(textNode)
-// }
-// for(let i = 0; i < 2; i++){
-//     createMongooses()
-// }
